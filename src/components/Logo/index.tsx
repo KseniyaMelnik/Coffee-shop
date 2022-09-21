0cc8aa61ae1cd70cc1f945322ee70432e99e9eb2
Kseniya Melnik <kseniyamelnnik@gmail.com>
2022-09-21 12:09:25 +0300

    feat: add deploy

--- src/components/Logo/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import LogoImg from "./../../assets/images/logo.png";
-
-
-export const Logo = () => {
-    return <div>
-        <img src={LogoImg} alt="logo" />
-    </div>
-}
